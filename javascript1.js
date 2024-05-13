86936afec2030a810316713b638181f9c4f1d594
Victor <160278295+VictoryMa8@users.noreply.github.com>
2024-05-13 18:30:22 -0500

    Changes

--- javascript1.js
+++ javascript1.js
@@ -34,4 +34,21 @@ function onImageClick6(){
 const snapchat = document.getElementById('snapchat');
 snapchat.addEventListener('click', onImageClick6);
 
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  var scrollToTopBtn = document.getElementById("scrollToTop");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
