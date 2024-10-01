9893d4efa8b63614904ee34da60e20e5c1ce193a
Victor <160278295+VictoryMa8@users.noreply.github.com>
2024-10-01 08:41:59 -0500

    C exercise

--- miscellaneous/hello.c
+++ miscellaneous/hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello World!");
+int num1;
+
+int main(void) {
+    printf("Hello World!\n");
+    printf("Enter a number: ");
+    scanf("%d", &num1);
+    printf("%d", num1);
     return 0;
 }
